39304738ef5fc5d404c179de25796ac007d83d79
Felix Matschke <felix.matschke@gmail.com>
2019-01-21 09:03:46 -0500

    Change to tag 0.1.0

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "LSM303",
-            dependencies: []),
+            dependencies: ["SwiftyGPIO"]),
         .testTarget(
             name: "LSM303Tests",
             dependencies: ["LSM303"]),
